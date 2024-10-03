5fdd1fd195a5eff826e90f6ad1bbd6fd4ba3bb62
iarosas <153024000+iarosas@users.noreply.github.com>
2024-10-03 11:52:42 +0200

    Update bucket.tf
    Creacion de 2 buckets con un provider random que me da un 
    nombre de bucket unico

--- AWS/Despliegue S3/bucket.tf
+++ AWS/Despliegue S3/bucket.tf
@@ -1,1 +1,19 @@
+resource "aws_s3_bucket" "libros" {
+  count  = 6
+  bucket = "proveedores-${random_string.sufijo[count.index].id}" // Nombre del bucket es unico en todo AWS, añadimos un random para que sea unico
+  tags = {
+    Owner       = "Julian"
+    Environment = "Dev"
+    Office      = "Biblioteca"
+  }
+}
 
+resource "aws_s3_bucket" "ordenadores" {
+  bucket = "ordenadores-${random_string.sufijo[count.index].id}"
+  count  = 6
+  tags = {
+    Owner       = "Julian" 
+    Name        = "Dev"
+    Environment = "Office"
+  }
+}
